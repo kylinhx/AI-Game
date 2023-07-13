3750b1230a5d7afd10736fc2c76d7d3302de8380
kylinhx <12261615+kylinhx@user.noreply.gitee.com>
2023-07-13 15:51:05 +0800

    7.13

--- The_King_of_Fighters_XV/DQN.py
+++ The_King_of_Fighters_XV/DQN.py
@@ -15,7 +15,7 @@ from GameENV import ENV
 bbox_detector_path = './The_King_of_Fighters_XV/model/bbox_detector.pt'
 Action_detector_path = './The_King_of_Fighters_XV/model/action_detector.pt'
 
-# Define the DQN network
+# DQN网络
 class DQN(nn.Module):
     def __init__(self, output_dim):
         super(DQN, self).__init__()
@@ -29,7 +29,7 @@ class DQN(nn.Module):
         x = self.mobilev3.classifier(x)
         return x
 
-# Define Experience Replay buffer
+# 经验回放
 class ReplayBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -37,21 +37,13 @@ class ReplayBuffer:
         self.position = 0
 
     def push(self, state, action, reward, next_state, done):
-        # Add new experience to the buffer
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
         self.buffer[self.position] = (state, action, reward, next_state, done)
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
-        # Sample a batch from the buffer
         batch = random.sample(self.buffer, batch_size)
-        # state, action, reward, next_state, done = map(lambda x: x.cpu().numpy(), zip(*batch))
-        # state, action, reward, next_state, done = map(lambda x: x.cuda(), zip(*batch))
-        print(batch)
-        print(type(batch))
-        
-        # state, action, reward, next_state, done = [torch.tensor(x).cuda() for x in zip(*batch)]
         state, action, reward, next_state, done = [torch.tensor(x).squeeze().cuda().item() if torch.is_tensor(x) else x for x in zip(*batch)]
         return state, action, reward, next_state, done
 
@@ -60,7 +52,6 @@ class ReplayBuffer:
 
 class DQNAgent:
     def __init__(self, env):
-        # Initialize agent with environment
         self.env = env
 
         self.input_dim = 15   # 获取状态空间维度
@@ -68,14 +59,12 @@ class DQNAgent:
 
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 检查是否有GPU
 
-        # Define DQN networks and optimizer
         self.policy_net = DQN(self.output_dim).to(self.device)   # 创建策略网络
         self.target_net = DQN(self.output_dim).to(self.device)   # 创建目标网络
         self.target_net.load_state_dict(self.policy_net.state_dict())   # 将策略网络的参数复制到目标网络中
         self.target_net.eval()   # 将目标网络设置为评估模式
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=0.001)   # 创建Adam优化器
 
-        # Define hyperparameters
         self.gamma = 0.99   # 奖励折扣因子
         self.epsilon = 1.0   # ε-greedy策略的初始探索率
         self.epsilon_min = 0.01   # ε-greedy策略的最小探索率
@@ -84,28 +73,20 @@ class DQNAgent:
         self.memory = ReplayBuffer(10000)   # 创建经验回放缓冲区
 
     def select_action(self, state):
-        # Choose an action using epsilon-greedy
         if np.random.rand() < self.epsilon:
-            # Select a random action with probability ε
             return self.env.select_randomAction()
         
         with torch.no_grad():
-            # Select the action with the highest Q-value with probability (1-ε)
-            # return self.env.select_randomAction()
-            # print(state)
             state = torch.tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0)
             q_values = self.policy_net(state.view(-1,3,self.input_dim//3, 1))
             action = q_values.max(1)[1].item()
-
         return action
 
     def train(self):
-        # Train the network using sampled experiences
         if len(self.memory) < self.batch_size:
-            # If the replay buffer does not have enough experiences, skip training
+            # 如果小于batchsize，跳过训练
             return
 
-        # Sample a batch of experiences from the replay buffer
         state, action, reward, next_state, done = self.memory.sample(self.batch_size)
         state = torch.FloatTensor(state).to(self.device)
         action = torch.LongTensor(action).to(self.device)
@@ -113,31 +94,27 @@ class DQNAgent:
         next_state = torch.FloatTensor(next_state).to(self.device)
         done = torch.FloatTensor(done).to(self.device)
 
-        # Compute the Q-values for the current state-action pairs and the next states
-        print(state.shape)
+        # 计算当前状态-操作对和下一个状态的 Q 值
         q_values = self.policy_net(state.view(-1,3,self.input_dim//3,1)).gather(1, action.unsqueeze(1)).squeeze(1)
         next_q_values = self.target_net(next_state.view(-1,3,self.input_dim//3,1)).max(1)[0]
 
-        # Compute the expected Q-values using the Bellman equationd
+        # 使用贝尔曼方程计算预期的 Q 值
         expected_q_values = reward + self.gamma * next_q_values * (1 - done)
 
-        # Compute the loss between the predicted Q-values and the expected Q-values
+        # 计算预测 Q 值与预期 Q 值之间的损耗
         loss = nn.MSELoss()(q_values, expected_q_values.detach())
 
         print("train start")
-        # Update the weights of the policy network using backpropagation
+        # 使用反向传播更新策略网络的权重
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
     def update_target_model(self):
-        # Update the target network with the policy network's weights
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
     def run(self, episodes):
-        # Run the agent for a specified number of episodes
         for episode in range(episodes):
-            # Reset the environment for a new episode
             state = self.env.get_state()
             done = False
             total_reward = 0
@@ -147,7 +124,7 @@ class DQNAgent:
                 print(f"action: {action}")
                 # Take a step in the environment with the selected action
                 next_state, reward, done = self.env.step(action)
-
+                
                 # Store the experience in the replay buffer
                 self.memory.push(state, action, reward, next_state, done)
 
@@ -158,13 +135,8 @@ class DQNAgent:
                 state = next_state
                 total_reward += reward
 
-            # Update the ε-greedy exploration rate
             self.epsilon = max(self.epsilon_min, self.epsilon_decay * self.epsilon)
-
-            # Update the target network with the weights of the policy network
             self.update_target_model()
-
-            # Print the episode number, the total reward, and the current ε-greedy exploration rate
             print('Episode: {}/{}, Total reward: {}, Epsilon: {:.2f}'
                   .format(episode+ 1, episodes, total_reward, self.epsilon))
 
@@ -174,11 +146,11 @@ if __name__ == "__main__":
     Action_detector = YOLO(Action_detector_path)
 
     env = ENV(
-        window_size=(0,0,1050,720),
+        window_size=(0,0,990,560),
         bbox_detector = Box_detector,
         action_detector = Action_detector,
     )
 
     dqn_agent = DQNAgent(env)
 
-    dqn_agent.run(episodes=1)
+    dqn_agent.run(episodes=2)
